75bb5c39c77e0d71291a75b5de7263420a04da4b
Shea Phillips <shea.phillips@gmail.com>
2021-02-12 12:26:21 -0800

    - removing DNS elements for tnfhhm (CPF) account; will instead be handled in account provisioning automation where it belongs

--- main.tf
+++ main.tf
@@ -41,34 +41,3 @@ resource "aws_route53_zone" "nimbus" {
 	provider = aws.perimeter
 	name = "nimbus.cloud.gov.bc.ca"
 }
-
-//@todo we probably want to create the project account zone and NS records as part of the account creation automation for team accounts
-resource "aws_route53_zone" "tnfhhm-dev" {
-	provider = aws.perimeter
-	name = "tnfhhm-dev.nimbus.cloud.gov.bc.ca"
-
-	tags = {
-		Environment = "dev"
-		Project = "tnfhhm"
-	}
-}
-
-resource "aws_route53_record" "tnfhhm-dev-ns" {
-	provider = aws.perimeter
-	zone_id = aws_route53_zone.nimbus.zone_id
-	name    = "tnfhhm-dev.nimbus.cloud.gov.bc.ca"
-	type    = "NS"
-	ttl     = "30"
-	records = aws_route53_zone.tnfhhm-dev.name_servers
-}
-
-//@todo this should be created as part of an application's infrastructure
-resource "aws_route53_record" "tnfhhm-dev-sample-app-cname" {
-	provider = aws.perimeter
-	zone_id = aws_route53_zone.tnfhhm-dev.zone_id
-	name    = "ssp-sample.tnfhhm-dev.nimbus.cloud.gov.bc.ca"
-	type    = "CNAME"
-	ttl     = "30"
-	// todo source this from a datasource
-	records = ["Public-DevTest-perimeter-alb-36523785.ca-central-1.elb.amazonaws.com"]
-}
