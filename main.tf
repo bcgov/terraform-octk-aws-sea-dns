c2e46645bae008d49d5f2fe351524d6b1adf4151
Prabhu <70853875+prabhukiran9999@users.noreply.github.com>
2023-07-24 20:04:55 -0400

    Remove provider config from the module

--- main.tf
+++ main.tf
@@ -1,33 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  version = "~> 4.0"
-  region  = "ca-central-1"
-  alias   = "master"
-}
-
-provider "aws" {
-  version = "~> 4.0"
-  region  = "ca-central-1"
-  alias   = "perimeter"
-
-  assume_role {
-    role_arn     = "arn:aws:iam::${local.security_account.id}:role/${var.automation_role_name}"
-    session_name = "slz-terraform-automation"
-  }
-}
-
 module "lz_info" {
   source = "github.com/BCDevOps/terraform-aws-sea-organization-info"
   providers = {
-    aws = aws.master
+    aws = aws.master-account
   }
 }
 
@@ -38,6 +12,6 @@ locals {
 
 
 resource "aws_route53_zone" "nimbus" {
-  provider = aws.perimeter
+  provider = aws.Perimeter-account
   name     = "nimbus.cloud.gov.bc.ca"
 }
